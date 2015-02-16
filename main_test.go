eb3a2779dea408142056209050c3a79757183945
Willa Walthall <ghthor@gmail.com>
2015-02-16 16:52:10 -0500

    [@ghthor, @vise890] add blank line after header

--- main_test.go
+++ main_test.go
@@ -1,6 +1,8 @@
 package mdfmt
 
 import (
+	"bufio"
+	"bytes"
 	"io"
 	"io/ioutil"
 	"os"
@@ -74,6 +76,35 @@ func TestFmt(t *testing.T) {
 
 }
 
-func Fmt(in io.Reader) (out io.Reader) {
-	return in
+func Fmt(input io.Reader) (output io.Reader) {
+	scanner := bufio.NewScanner(input)
+	scanner.Split(bufio.ScanLines)
+
+	out := bytes.NewBuffer(make([]byte, 0, 1024))
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if line[0] == '#' {
+			out.WriteString(line + "\n")
+			mustBeFollowedByBlankLine(scanner, out)
+		}
+	}
+
+	return out
+}
+
+func mustBeFollowedByBlankLine(scanner *bufio.Scanner, out io.Writer) {
+	scanner.Split(bufio.ScanLines)
+
+	if scanner.Scan() {
+		if scanner.Text() == "" {
+			out.Write([]byte("\n"))
+		} else {
+			out.Write([]byte("\n"))
+			out.Write([]byte(scanner.Text() + "\n"))
+		}
+	}
+
+	return
 }
